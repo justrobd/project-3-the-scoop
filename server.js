a47e45bf253d1a52e859449067699a74928d2b7d
Robert DeBartolo <robertdebartolo@gmail.com>
2018-02-24 16:04:40 -0600

    Add udpateComment function and PUT method to routes

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ const routes = {
     'POST': createComment 
   }, 
   '/comments/:id': {
+    'PUT': updateComment
   },
   '/comments/:id/upvote': {
   },
@@ -275,6 +276,27 @@ function createComment (url, request) {
          response.status = 400;
      }
     return response;
+}
+
+function updateComment (url, request) {
+    const id = Number(url.split('/').filter(segment => segment)[1]);
+    const savedComment = database.comments[id];
+    const requestComment = request.body && request.body.comment;
+    const response = {};
+
+    if(!id || !requestComment ) {
+       response.status = 400; 
+    } else if (!savedComment) {
+       response.status = 404; 
+    } else {
+        savedComment.body = requestComment.body || savedComment.body;
+        savedComment.url = requestComment.url || savedComment.url;
+
+        response.body = {comment: savedComment};
+        response.status = 200;
+    } 
+    return response;
+
 }
 
 // Write all code above this line.
